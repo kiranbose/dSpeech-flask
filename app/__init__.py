00c59acf1834312b2df38b9b7653aa8e8004b74b
Kiran <kiranbose@gmail.com>
2019-02-04 22:30:34 +0530

    fix5

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,8 @@
 from flask import Flask
-from flask_socketio import SocketIO
 
 app = Flask(__name__)
-socketIO = SocketIO(app)
 
 from app import routesHandler
- 
+
 if (__name__ == "app"):
     app.run()
-    socketIO.run(app)
