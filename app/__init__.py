2980ae4776411e31dc1d5c040692cda2a055b88e
Kiran <kiranbose@gmail.com>
2019-02-04 20:43:45 +0530

    fix 2

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_socketio import SocketIO
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 socketIO = SocketIO(app)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 if (__name__ == "app"):
     app.run()
