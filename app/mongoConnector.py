87823a680694ec93c9045ff017ce3797a06fd0e3
Kiran <kiranbose@gmail.com>
2019-02-26 17:20:36 +0530

    query change

--- app/mongoConnector.py
+++ app/mongoConnector.py
@@ -18,7 +18,7 @@ def prepareResponse(data):
 
 
 def retrieve(email):
-    data = routesHandler.recordingsCollection.find({'user.email': email})
+    data = routesHandler.recordingsCollection.find({'user.email' : {'$in': ['admin', email]}})
     if routesHandler.recordingsCollection.count() == 0:
         data = []
     return prepareResponse(data)
